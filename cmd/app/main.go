817491363996b2614a55c0556732fef42015d7e0
kingxl111 <kingxl111@mail.ru>
2024-10-23 23:12:33 +0300

    loading from config added

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,10 +1,18 @@
 package main
 
+import (
+	"cakes-database-app/pkg/config"
+	"log"
+)
+
+
+
 func main() {
+	// TODO: config - cleanenv
+	cfg := config.MustLoad()
+	log.Printf("%s", cfg.DB.Address)
 
 	// TODO: database implementation!
-
-	// TODO: config
 
 	// TODO: logger
 
--- pkg/api/api.go
+++ pkg/api/api.go
@@ -2,6 +2,11 @@ package api
 
 import "cakes-database-app/pkg/storage"
 
+// dependency inversion principle
 type API struct {
-	db *storage.Interface
+	db storage.Interface
+}
+
+func New(db storage.Interface) *API {
+	return &API{db: db}
 }
